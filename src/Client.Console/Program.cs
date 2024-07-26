a8d024e1f0d2adb884229875752441d5ccbf7cce
wodson <wodsonluiz@live.com>
2024-07-26 09:28:09 -0300

    feat: Add server and client

---
+++ src/Client.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Client.Console;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            TcpClient client = new("127.0.0.1", 6000);
+            Console.WriteLine("Connected client");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            string responseData = Encoding.ASCII.GetString(data, 0, bytes);
+
+            System.Console.WriteLine($"Receive: {responseData}");
+
+            client.Close();
+
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ src/ServerTCP.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ServerTCP.Console;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = null;
+
+        try
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            server = new(localAddr, 6000);
+            server.Start();
+
+            Console.WriteLine("Server inited, waiting connections...");
+
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected!");
+
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = Encoding.ASCII.GetBytes("Hello client!");
+                stream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Message send to client");
+
+                client.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            server.Stop();
+        }
+    }
+}
